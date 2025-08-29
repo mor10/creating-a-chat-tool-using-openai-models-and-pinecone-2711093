05037b4a838e2d567eed4f4e6f11e1add9f4590c
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2025-08-29 22:41:39 +0000

    let's see!

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 import OpenAI from "openai";
 
+// Choose between GitHub Models or OpenAI API. Default "github"
+export const MODEL_PROVIDER = "github"; // "github" or "openai"
+
+const token = import.meta.env.VITE_GITHUB_TOKEN;
+const endpoint = "https://models.github.ai/inference";
+console.log(token)
 // OpenAI config
 export const openai = new OpenAI({
-  apiKey: "OPENAI_API_KEY",
+  ...(MODEL_PROVIDER === "github" ? { baseURL: endpoint } : {}),
+  apiKey: MODEL_PROVIDER === "github" ? token : "OPENAI_API_KEY",
   dangerouslyAllowBrowser: true,
 });
